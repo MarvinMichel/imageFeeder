342aa0158ceaff0ccdcb9fe7096e88c9fd54679f
MarvinMichel <mmhsernee@gmail.com>
2021-02-05 14:07:27 +0100

    Create generic data function

--- js/app.js
+++ js/app.js
@@ -1,27 +1,11 @@
-(function () {
-  const apiKey = 'lbhvcWW7O-NY5XERiGF9BrvC2L05e-MWIjagBBsXr9A'
+import { getImageData } from "./modules/getImageData.js"
+
+(function() {
+  getImageData('photos/random')
 
   const searchForm = document.querySelector('form')
   searchForm.addEventListener('submit', e => {
     e.preventDefault()
-    if (search.value) searchImages(search.value)
+    if (search.value) getImageData('search/photos/', search.value)
   })
-
-  function getRandomImage() {
-    const apiURL = 'https://api.unsplash.com/photos/random/'
-  
-    return fetch(`${apiURL}?client_id=${apiKey}`)
-      .then(res => res.json())
-      .then(data => console.log(data.urls.regular))
-      .catch(err => console.log('Something went wrong', err))
-  }
-  
-  function searchImages(query) {
-    const apiURL = 'https://api.unsplash.com/search/photos/'
-  
-    return fetch(`${apiURL}?query=${query}&client_id=${apiKey}`)
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.log('Something went wrong', err))
-  }
 })()
